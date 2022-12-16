160426ab6b32dca1af1a6e9d9af7bffd447b7a02
MatteoVarano <matteovarano06@gmail.com>
2022-12-16 12:11:15 +0000

    a

---
+++ EsVerifica/ES1verifica.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main()
+{
+    int num1,num2,cont;
+
+    printf("inserisci due numeri interi");
+    scanf("%d",&num1);
+
+    for(cont=num1;cont<num2;cont++)
+    {
+        printf("%d",cont);
+    }
+
+
+}
