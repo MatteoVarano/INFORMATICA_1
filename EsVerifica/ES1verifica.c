72c80f2b20a519bbbe278df40eb36617c9980d5a
MatteoVarano <matteovarano06@gmail.com>
2022-12-16 12:34:20 +0000

    a

--- EsVerifica/ES1verifica.c
+++ EsVerifica/ES1verifica.c
@@ -2,15 +2,17 @@
 
 int main()
 {
-    int num1,num2,cont;
+    int num1,num2,cont,somma=0;
 
-    printf("inserisci due numeri interi");
-    scanf("%d",&num1);
-
-    for(cont=num1;cont<num2;cont++)
+    printf("inserisci due numeri interi \n");
+    scanf("%d \n",&num1);
+    scanf("%d",&num2);
+    
+     for(cont=num1;cont<=num2;cont++)
     {
-        printf("%d",cont);
+        printf("\n %d \n",cont);
+        somma+=cont;
     }
-
-
+    printf("la somma e' %d \n",somma);
+    return 0;
 }
