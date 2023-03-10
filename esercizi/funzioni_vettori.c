9a2f19ecb94bfcc19dcc942c3d003362a8bb4f76
MatteoVarano <matteovarano06@gmail.com>
2023-03-10 12:54:59 +0000

    a

--- esercizi/funzioni_vettori.c
+++ esercizi/funzioni_vettori.c
@@ -2,23 +2,73 @@
 
 #define N 10
 
+int menu();
+
+int caricavettore(int v[]);
+
+void stampavettori(int v[]);
+
 void ordinavettore(int v[]);
-
-int contamultipli(int v[], int x);
-
-int contaprimi(int v[]);
 
 int main()
 {
-    int* v[N];
-    int cont=0,i;
+    int v[N];
+    int cont=0,i,r;
+
+        do
+    {
+        r=menu();
+        switch(r)
+        {
+            case 0: printf("Fine programma \n");
+                    break;
+            case 1: r=caricavettore(v);
+                    printf("\ni numeri nel vettore sono\t");
+                    stampavettori(v);
+                    printf("\ni numeri nel vettore ordinati sono\t");
+                    ordinavettore(v);
+                    stampavettori(v);
+            case 2: 
+            default: printf("hai inserito un numero sbagliato\n");                           
+        }
+    }
+    while(r!=0);
+
+    return 0;
+}
+
+
+int menu()
+{
+    int risp;
+    printf("Matteo Varano \n");
+    printf("\ndigita 0 se vuoi uscire dal programma\n");
+    printf("digita 1 per inserire i numeri da tastiera\n");
+    printf("digita 2 per caricare numeri randomici\n");
+    scanf("%d",&risp);
+    return risp;
+}
+
+int caricavettore(int v[])
+{
+    int cont,i;
     for(i=0;i<N;i++)
     {
-        printf("inserisci il %d numero nel vettore",cont);
-        scanf("%d",v[i]);
-        cont++;
+        printf("inserisci 10 numeri: ");
+        scanf("%d",&v[i]);
     }
-    ordinavettore(v[]);
+    return *v;
+}
+
+void stampavettori(int v[])
+{
+        for(int c = 0; c<N; c++)
+    {
+        printf("%d\t ", v[c]);
+    }
+    printf("\n\n");
+
+    
 }
 
 void ordinavettore(int v[])
