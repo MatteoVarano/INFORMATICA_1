b5dfe74cc9da0ccc138501c42fe00926337d9b35
MatteoVarano <matteovarano06@gmail.com>
2023-11-20 07:46:00 +0000

    a

--- Es_filerecord/file.c
+++ Es_filerecord/file.c
@@ -12,6 +12,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
+#include <string.h>
 
 #define N 1
 #define R 20
@@ -33,57 +34,84 @@ struct alunno
     data nascita;
 }typedef studente;
 
-void crea(char fin[],studente s[]);
+void crea(char fin[]);
 
-void stampa(char fin[],studente s[]);
+void stampa(char fin[]);
 
-void contacognome(char fin[],studente s[]);
+int contacognome(char fin[],char n[]);
 
-void media(char fin[],studente s[]);
+void media(char fin[]);
+
+int cercacognome(char fin[], char n[]);
+
+int contarecord(char fin[]);
 
 int main()
 {
-    studente s[N];
+    int r;
+    char n[R];
+    studente s;
 
-    crea("file.dat",s);
+    crea("file.dat");
 
-    stampa("file.dat",s);
+    stampa("file.dat");
+
+    r=contacognome("file.dat","varano");
+    printf("cognomi trovati %d\n\n",r);
+
+    media("file.dat");
+
+    printf("inserisci il cognome da trovare: ");
+    scanf("%s",n);
+    r=cercacognome("file.dat",n);
+    if(r!=-1)
+    {
+        printf("il cognome e' stato trovato il posizione:%d\n",r);
+    }
+    else
+    {
+        printf("il cognome non e' stato trovato");
+    }
+
+    r=contarecord("file.dat");
+    printf("i record sono %d\n",r);    
 
     return 0;
 }
 
-void crea(char fin[],studente s[])
+void crea(char fin[])
 {
+    studente s;
     FILE * err1;
     err1=fopen(fin,"ab");
-    int t;
+    
 
     if(err1 != 0)
     {
         for(int i=0;i<N;i++)
         {
-            t=i+1;
-            s[i].matricole = t;
+            int t=i+1;
+            s.matricole = t;
 
             printf("\ninserisci il nome\n");
-            scanf("%s",s[i].nome);
+            scanf("%s",s.nome);
 
             printf("inserisci il cognome\n");
-            scanf("%s",s[i].cognome);
+            scanf("%s",s.cognome);
+
+            printf("inserisci il giorno di nascita\n");
+            scanf("%d",&s.nascita.giorno);
+
+            printf("inserisci il mese di nascita\n");
+            scanf("%s",s.nascita.mese);
+
+            printf("inserisci il anno di nascita\n");
+            scanf("%d",&s.nascita.anno);
 
             for(int j=0; j<V;j++)
             {
-                s[i].voti[j] = rand()%10+1;
+                s.voti[j] = rand()%10+1;
             }
-
-            printf("inserisci il giorno di nascita\n");
-            scanf("%d",&s[i].nascita.giorno);
-
-            printf("inserisci il mese di nascita\n");
-            scanf("%s",s[i].nascita.mese);
-
-            printf("inserisci il anno di nascita\n");
-            scanf("%d",&s[i].nascita.anno);
 
             fwrite(&s, sizeof(studente), 1, err1);
         }
@@ -97,29 +125,134 @@ void crea(char fin[],studente s[])
 
 }
 
-void stampa(char fin[],studente s[])
+void stampa(char fin[])
 {
+    studente s;
     FILE * err1;
-    err1=fopen(fin,"rb");
-
-    if(err1 != 0)
+    err1=fopen(fin, "rb");
+    if(err1!=NULL)
     {
-        for(int i=0;i<N;i++)
+        while(!feof(err1))
         {
             fread(&s, sizeof(studente), 1, err1);
-            printf("\nnumero matricola: %d\n",s[i].matricole);
-            printf("cognome: %s\n",s[i].cognome);
-            printf("nome: %s\n",s[i].nome);
-            printf("data di nascita: %d %s %d\n", s[i].nascita.giorno, s[i].nascita.mese, s[i].nascita.anno);
-            for(int j=0;j<V;j++)
+
+            printf("\nnumero matricola: %d\n", s.matricole);
+            printf("cognome: %s\n", s.cognome);
+            printf("nome: %s\n", s.nome);
+            printf("giorno di nascita: %d/%s/%d\n", s.nascita.giorno, s.nascita.mese, s.nascita.anno);
+            printf("voti\n");
+            for(int j = 0; j<V; j++){
+                printf("%d\t", s.voti[j]);
+            }
+            printf("\n\n");
+        }
+    }
+    fclose(err1);
+}
+
+int contacognome(char fin[],char n[])
+{
+    studente s;
+    FILE * err1; 
+    err1=fopen(fin, "rb");
+
+    int c = 0;
+
+    if(err1 != NULL)
+    {
+        while(!feof(err1))
+        {
+            fread(&s, sizeof(studente), 1, err1);
+
+            if(strcmp(s.cognome, n) == 0) 
+            c++;
+        }
+    }
+    fclose(err1);
+
+    return c; 
+}
+
+void media(char fin[])
+{
+    studente s;
+    FILE * err1;
+    err1=fopen(fin,"rb");
+    int j,media = 0, max = 0 , min = 10;
+
+    if(err1!=NULL)
+    {
+        while(!feof(err1))
+        {
+            fread(&s, sizeof(studente), 1, err1);
+
+            printf("cognome:%s\n",s.cognome);
+            for(j=0;j<V;j++)
             {
-                printf("voti: %d\n",s[i].voti[j]);
+                media+=s.voti[j];
+
+                if(max<s.voti[j])
+                {
+                    max=s.voti[j];
+                }
+                if(min>s.voti[j])
+                {
+                    min=s.voti[j];
+                }
+            }
+            
+            media/=V;
+
+            printf("media dei voti: %d\n", media);
+            printf("massimo dei voti: %d\n", max);
+            printf("minimo dei voti: %d\n\n", min);
+
+            media = 0;
+            max = 0;
+            min = 10;            
+
+        }
+    }
+    fclose(err1);    
+}
+
+int cercacognome(char fin[], char n[])\
+{
+    studente s;
+    FILE * err1; 
+    err1=fopen(fin, "rb");
+
+    int c = -1;
+
+    if(err1 != NULL)
+    {
+        while(!feof(err1))
+        {
+            fread(&s, sizeof(studente), 1, err1);
+
+            if(strcmp(s.cognome, n)==0) 
+            {
+                c = ftell(err1) / sizeof(studente); 
             }
         }
-        fclose(err1);
     }
-    else
+    fclose(err1);
+
+    return c; 
+}
+
+int contarecord(char fin[])
+{
+    FILE * err1;
+    err1=fopen(fin, "rb");
+
+    int d = -1;
+
+    if(err1!=NULL)
     {
-        printf("file non aperto");
+        fseek(err1, 0, SEEK_END);
+        d=ftell(err1);
     }
+
+    return d/sizeof(studente);    
 }
