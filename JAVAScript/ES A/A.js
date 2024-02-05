0451d831118ae8557663367efc8b7e5f43ed0b2e
MatteoVarano <114076428+MatteoVarano@users.noreply.github.com>
2024-02-05 09:22:04 +0100

    Add files via upload

---
+++ JAVAScript/ES A/A.js
@@ -0,0 +1,9 @@
+scatola = {}
+
+scatola.larghezza = prompt("inserisci la larghezza ")
+scatola.altezza = prompt("inserisci l'altezza")
+scatola.lunghezza = prompt("inserisci la lunghezza")
+
+volume=scatola.larghezza*scatola.altezza*scatola.lunghezza
+
+console.log("volume della scatola=" + volume)
