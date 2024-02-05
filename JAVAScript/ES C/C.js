ec8cab0245b2b0ecb85d0c49696df3fb102b2eb7
MatteoVarano <matteovarano06@gmail.com>
2024-02-05 09:00:11 +0000

    a

---
+++ JAVAScript/ES C/C.js
@@ -0,0 +1,10 @@
+Utente = {}
+
+Utente.nome = prompt("inserisci il tuo nome ")
+Utente.cognome = prompt("inserisci il tuo cognome ")
+Utente.eta = prompt("inserisci la tua eta ")
+
+console.log("Utente")
+console.log("Nome: " +Utente.nome)
+console.log("Cognome: " +Utente.cognome)
+console.log("Eta: " +Utente.eta)
