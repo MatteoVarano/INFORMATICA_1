71652cb8e44b2db7909786dcd7d8c035093501be
MatteoVarano <114076428+MatteoVarano@users.noreply.github.com>
2024-02-05 09:45:28 +0100

    Add files via upload

---
+++ JAVAScript/ES B/B.js
@@ -0,0 +1,12 @@
+Retta = {}
+
+Retta.m = prompt("inserisci la m per calcolare il punto y")
+Retta.c = prompt("inserisci la c per calcolare il punto y")
+Retta.x1 = -1
+Retta.x2 = 10
+
+y=Retta.m*Retta.x1+(Retta.c*1)
+y2=Retta.m*Retta.x2+(Retta.c*1)
+
+console.log("nel punto con x=-1 e' =" + y)
+console.log("nel punto con x=10 e'=" + y2)
