8acdad71e3a2b367d1ed898045712466f5dcb4ca
MatteoVarano <matteovarano06@gmail.com>
2024-03-27 12:34:54 +0000

    a

--- JAVAScript/ES04/ES B/B.js
+++ JAVAScript/ES04/ES B/B.js
@@ -1,2 +1,24 @@
-numero = parseInt(prompt("quanti numeri desideri inserire"));
+alert("inserire un numero con almeno 2 cifre");
+numero = parseInt(prompt("quanti numeri desideri inserire"))
 
+if(Number.isNaN(numero)){
+  alert("numero inserito non valido")
+}
+
+inverti(numero)
+
+function inverti(num){
+    
+    let reversed = 0;
+    while (num !== 0) {
+        
+        let digit = num % 10;
+      
+        reversed = reversed * 10 + digit;
+      
+        num = parseInt(num / 10);
+      }  
+
+      console.log(reversed);
+
+}
